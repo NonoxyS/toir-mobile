eabbac6b56ff07a7ad7749c62824482220dffa64
Andrey Nalimov <ae.nalimov@gmail.com>
2026-04-11 20:36:00 +0300

    feat(theme): add Inter font family

--- shared/common-ui/src/commonMain/kotlin/ru/mirea/toir/common/ui/compose/theme/Fonts.kt
+++ shared/common-ui/src/commonMain/kotlin/ru/mirea/toir/common/ui/compose/theme/Fonts.kt
@@ -7,6 +7,15 @@ import androidx.compose.ui.text.font.FontWeight
 import dev.icerock.moko.resources.compose.asFont
 import ru.mirea.toir.res.MR
 
+val fontInter: FontFamily
+    @Composable get() = FontFamily(
+        MR.fonts.inter_regular.asFont(weight = FontWeight.Normal).requireNotNull(),
+        MR.fonts.inter_medium.asFont(weight = FontWeight.Medium).requireNotNull(),
+        MR.fonts.inter_semibold.asFont(weight = FontWeight.SemiBold).requireNotNull(),
+        MR.fonts.inter_bold.asFont(weight = FontWeight.Bold).requireNotNull(),
+        MR.fonts.inter_thin.asFont(weight = FontWeight.Thin).requireNotNull(),
+    )
+
 val fontRoboto: FontFamily
     @Composable get() = FontFamily(
         MR.fonts.roboto_regular.asFont(weight = FontWeight.Normal).requireNotNull(),
