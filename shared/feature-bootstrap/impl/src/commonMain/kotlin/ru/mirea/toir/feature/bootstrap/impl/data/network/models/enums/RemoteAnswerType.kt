0d08d9c5215156be2ee93f25282af5a03257312f
Andrey Nalimov <ae.nalimov@gmail.com>
2026-04-17 13:22:01 +0300

    fix: convert Remote enum serializers from val to object (annotation class literal fix)

--- shared/feature-bootstrap/impl/src/commonMain/kotlin/ru/mirea/toir/feature/bootstrap/impl/data/network/models/enums/RemoteAnswerType.kt
+++ shared/feature-bootstrap/impl/src/commonMain/kotlin/ru/mirea/toir/feature/bootstrap/impl/data/network/models/enums/RemoteAnswerType.kt
@@ -1,6 +1,9 @@
 package ru.mirea.toir.feature.bootstrap.impl.data.network.models.enums
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import ru.mirea.toir.core.network.deserializer.enumSerializerWithFallback
 
 @Serializable(with = RemoteAnswerTypeSerializer::class)
@@ -13,13 +16,18 @@ internal enum class RemoteAnswerType {
     UNKNOWN,
 }
 
-internal val RemoteAnswerTypeSerializer = enumSerializerWithFallback(
-    fallbackEnum = RemoteAnswerType.UNKNOWN,
-    serialNameMapping = mapOf(
-        RemoteAnswerType.BOOLEAN to "boolean",
-        RemoteAnswerType.NUMBER to "number",
-        RemoteAnswerType.TEXT to "text",
-        RemoteAnswerType.SELECT to "select",
-        RemoteAnswerType.CONFIRM to "confirm",
+internal object RemoteAnswerTypeSerializer : KSerializer<RemoteAnswerType> {
+    private val delegate = enumSerializerWithFallback(
+        fallbackEnum = RemoteAnswerType.UNKNOWN,
+        serialNameMapping = mapOf(
+            RemoteAnswerType.BOOLEAN to "boolean",
+            RemoteAnswerType.NUMBER to "number",
+            RemoteAnswerType.TEXT to "text",
+            RemoteAnswerType.SELECT to "select",
+            RemoteAnswerType.CONFIRM to "confirm",
+        )
     )
-)
+    override val descriptor = delegate.descriptor
+    override fun serialize(encoder: Encoder, value: RemoteAnswerType) = delegate.serialize(encoder, value)
+    override fun deserialize(decoder: Decoder): RemoteAnswerType = delegate.deserialize(decoder)
+}
--- shared/feature-bootstrap/impl/src/commonMain/kotlin/ru/mirea/toir/feature/bootstrap/impl/data/network/models/enums/RemoteAssignmentStatus.kt
+++ shared/feature-bootstrap/impl/src/commonMain/kotlin/ru/mirea/toir/feature/bootstrap/impl/data/network/models/enums/RemoteAssignmentStatus.kt
@@ -1,6 +1,9 @@
 package ru.mirea.toir.feature.bootstrap.impl.data.network.models.enums
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import ru.mirea.toir.core.network.deserializer.enumSerializerWithFallback
 
 @Serializable(with = RemoteAssignmentStatusSerializer::class)
@@ -11,11 +14,16 @@ internal enum class RemoteAssignmentStatus {
     UNKNOWN,
 }
 
-internal val RemoteAssignmentStatusSerializer = enumSerializerWithFallback(
-    fallbackEnum = RemoteAssignmentStatus.UNKNOWN,
-    serialNameMapping = mapOf(
-        RemoteAssignmentStatus.ASSIGNED to "assigned",
-        RemoteAssignmentStatus.IN_PROGRESS to "in_progress",
-        RemoteAssignmentStatus.COMPLETED to "completed",
+internal object RemoteAssignmentStatusSerializer : KSerializer<RemoteAssignmentStatus> {
+    private val delegate = enumSerializerWithFallback(
+        fallbackEnum = RemoteAssignmentStatus.UNKNOWN,
+        serialNameMapping = mapOf(
+            RemoteAssignmentStatus.ASSIGNED to "assigned",
+            RemoteAssignmentStatus.IN_PROGRESS to "in_progress",
+            RemoteAssignmentStatus.COMPLETED to "completed",
+        )
     )
-)
+    override val descriptor = delegate.descriptor
+    override fun serialize(encoder: Encoder, value: RemoteAssignmentStatus) = delegate.serialize(encoder, value)
+    override fun deserialize(decoder: Decoder): RemoteAssignmentStatus = delegate.deserialize(decoder)
+}
